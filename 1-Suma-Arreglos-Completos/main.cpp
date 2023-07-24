93af3a2f61b6adb7d71daf1dc0fda50ab4084c25
Luis Ángel Cruz Díaz <luisangelcruzdiaz@icloud.com>
2023-07-23 21:02:42 -0600

    Compilamos los codigos en visual Studio Code para ahorrar espacio

--- 1-Suma-Arreglos-Completos/main.cpp
+++ 1-Suma-Arreglos-Completos/main.cpp
@@ -2,18 +2,23 @@
  *  Sumas sobre arreglos completos - Problema 1
  *  https://omegaup.com/arena/problem/Sumas-sobre-arreglos-completos/#problems
  *  Author: Luis Angel Cruz Díaz
+ *
  */
+
 #include <iostream>
 
 int main() {
     int n,m;
     std::cin >> n >> m;
     int arr[n];
+
     for(int i = 0; i < n;i++){
         std::cin >> arr[i];
     }
+
     int sum = 0;
     int temp;
+    
     for (int i = 0; i < m; ++i) {
         std::cin >> temp;
         sum += temp;
